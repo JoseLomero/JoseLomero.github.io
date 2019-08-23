c83c8e1528c448be5c68272c2f7fbe302d7c7350
JoseLomero <joselomero0@gmail.com>
2019-08-23 20:59:14 +0200

    tratando de hacer que se descarguen archivos

--- js/materialize-init.js
+++ js/materialize-init.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
     $('.parallax').parallax();
 });
 
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
 var instance = M.Parallax.getInstance(elem);
