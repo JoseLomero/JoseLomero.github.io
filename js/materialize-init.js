b469b19bbd10fceaaffd1f5bd47d99f11863b024
JoseLomero <joselomero0@gmail.com>
2021-02-01 06:02:08 +0100

    transformation to react

--- js/materialize-init.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.parallax').parallax();
-});
-
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-});
-
-var instance = M.Parallax.getInstance(elem);
