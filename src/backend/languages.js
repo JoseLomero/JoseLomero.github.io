38a81ff3a2f8af06fd506874ea21cb78f57f1285
JoseLomero <joselomero0@gmail.com>
2021-02-08 02:41:52 +0100

    languages index add

---
+++ src/backend/languages.js
@@ -0,0 +1,69 @@
+export default [{
+  "Basics": [
+    {
+      name: 'HTML5',
+      url: '/img/languages/html.png'
+    },
+    {
+      name: 'CSS3',
+      url: '/img/languages/css.png'
+    },
+    {
+      name: 'Javascript',
+      url: '/img/languages/javascript.png'
+    },
+  ],
+  "FrontEnd": [
+    {
+      name: 'React',
+      url: '/img/languages/react.png'
+    },
+    {
+      name: 'Angular',
+      url: '/img/languages/angular.png'
+    },
+    {
+      name: 'JQuery',
+      url: '/img/languages/jquery.png'
+    },
+  ],
+  "BackEnd": [
+    {
+      name: 'PHP',
+      url: '/img/languages/php.png'
+    },
+    {
+      name: 'Python',
+      url: '/img/languages/python.png'
+    },
+    {
+      name: 'Java',
+      url: '/img/languages/java.png'
+    },
+  ],
+  "Databases": [
+    {
+      name: 'SQL',
+      url: '/img/languages/sql.png'
+    },
+    {
+      name: 'MongoDB',
+      url: '/img/languages/mongodb.png'
+    },
+  ],
+  "Other Stuff": [
+    {
+      name: 'Git',
+      url: '/img/languages/git.png'
+    },
+    {
+      name: 'Bootstrap',
+      url: '/img/languages/bootstrap.png'
+    },
+    {
+      name: 'Wordpress',
+      url: '/img/languages/wordpress.png'
+    },
+  ],
+
+}]
