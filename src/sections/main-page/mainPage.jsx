e7a574dfe87f730f61f63bc93febd8eca096028e
JoseLomero <joselomero0@gmail.com>
2021-02-09 02:19:34 +0100

    main page updated

--- src/sections/main-page/mainPage.jsx
+++ src/sections/main-page/mainPage.jsx
@@ -34,8 +34,33 @@ class MainPage extends Component {
                         aos={"fade-right"}
                         aosEasing={"linear"}
                         aos-duration={"1500"}
-                        img={'/img/bothmon.png'}
+                        img={'/img/drawing.jpeg'}
+                        text={"Desarrollador de Aplicaciones Web entregado a su trabajo y siempre con ganas de aprender.Tengo una alta habilidad para expresarme gracias a mi formación como actor y locutor, y soy capaz de analizar y sintetizar problemas para ayudar a los demás a entenderlos. Me dedico a hacer las páginas web accesibles y sencillas de entender para los demás."}
+                        subtext={"Me gustaría conseguir un puesto de trabajo como FrontEnd developer, pero he trabajado ya como Fullstack Developer y BackEnd Developer, ¡así que puedes estar seguro que aceptaré el reto que me lances y lograré realizarlo!"}
                     />
+                    <Presentation
+                        aos={"fade-left"}
+                        aosEasing={"linear"}
+                        aos-duration={"1500"}
+                        img={'/img/drawing.jpeg'}
+                        title={"Pero... ¿cómo contactarme?"}
+                        text={"¡No te preocupes! Yo te cubro. Puedes descubrir más pulsando el botón de contacto de aqui o en el apartado corrrespondiente en el menú"}
+                        button={"Ir a Contacto"}
+                        buttonLink={"/contact"}
+                    />
+                    <Presentation
+                        aos={"fade-right"}
+                        aosEasing={"linear"}
+                        aos-duration={"1500"}
+                        img={'/img/drawing.jpeg'}
+                        title={"Pero... ¿cómo contactarme?"}
+                        text={"¡No te preocupes! Yo te cubro. Puedes descubrir más pulsando el botón de contacto de aqui o en el apartado corrrespondiente en el menú"}
+                        button={"Ir a Contacto"}
+                        buttonLink={"/contact"}
+                    />
+                    <div className="title">
+                        <h2 className="bullet-text">Pero, ¿qué es lo que conozco?</h2>
+                    </div>
                     <div className="languages-section">
                         <CardBootstrap bg="light" className="card-spacer">
                             <ImgGallery language={languages.basics} name={"Conocimientos Básicos"} />
@@ -53,6 +78,17 @@ class MainPage extends Component {
                             <ImgGallery language={languages.other} name={"Otros Conocimientos"} />
                         </CardBootstrap>
                     </div>
+
+                    <Presentation
+                        aos={"fade-left"}
+                        aosEasing={"linear"}
+                        aos-duration={"1500"}
+                        img={'/img/drawing.jpeg'}
+                        title={"¿Y cómo te contacto?"}
+                        text={"¡No te preocupes! Yo te cubro. Puedes descubrir más pulsando el botón de contacto de aqui o en el apartado corrrespondiente en el menú"}
+                        button={"Ir a Contacto"}
+                        buttonLink={"/contact"}
+                    />
                 </Row>
             </>
         );
